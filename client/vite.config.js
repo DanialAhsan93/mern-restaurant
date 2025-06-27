a6a543d050348967892dfb6da5a634b9106ced7f
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2025-06-27 23:45:19 +0500

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
-import path from 'path'
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), flowbiteReact()],
-  // server: {
-  //   host: '0.0.0.0',  // <- required
-  //   port: 5173,       // <- optional, but good to be consistent
-  //   strictPort: true,
-  // },
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, './src'),
-  //   },
-  // },
-  base: '/mern-restaurant/',
-  build: {
-    outDir: 'docs', // Specify the correct output directory
+  server: {
+    host: '0.0.0.0',  // <- required
+    port: 5173,       // <- optional, but good to be consistent
+    strictPort: true,
   },
+  // base: '/mern-restaurant/',
+  
 })
